835e97f008c9de74ac7e5543d2d540199e3d9713
yacoubdemire <yacoubdemire@gmail.com>
2023-03-25 11:10:42 +0100

    app custom

--- config/custom.php
+++ config/custom.php
@@ -4,7 +4,7 @@ return [
 
     'shopify_api_key' => env('SHOPIFY_API_KEY','9ce9a5bea07a8f9a1f504df72d37b546'),
     'shopify_api_secret' => env('SHOPIFY_API_SECRET','223acb85ecaec215a8fc91f3ef97db96'),
-    'shopify_api_version' => '2023-01',
+    'shopify_api_version' => '2023-07',
     'api_scopes' => 'write_orders,write_fulfillments,read_orders,write_customers,read_products,write_products,read_locations',
 
 ];
