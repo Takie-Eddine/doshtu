0fe7ab652fdc6d50abfeb530d2bb6d5470952ff9
yacoubdemire <yacoubdemire@gmail.com>
2023-03-29 10:53:25 +0100

    anufactory

--- resources/views/dashboard/companies/edit.blade.php
+++ resources/views/dashboard/companies/edit.blade.php
@@ -17,12 +17,12 @@
             <div class="content-header-left col-md-9 col-12 mb-2">
                 <div class="row breadcrumbs-top">
                     <div class="col-12">
-                        <h2 class="content-header-title float-start mb-0">Company</h2>
+                        <h2 class="content-header-title float-start mb-0">Manufactory</h2>
                         <div class="breadcrumb-wrapper">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a>
                                 </li>
-                                <li class="breadcrumb-item"><a href="{{route('admin.companies.index')}}">Companies</a>
+                                <li class="breadcrumb-item"><a href="{{route('admin.companies.index')}}">Manufactories</a>
                                 </li>
                                 <li class="breadcrumb-item active"><a >Edit</a>
                                 </li>
@@ -42,7 +42,7 @@
                         <div class="card">
                             @include('dashboard.layouts.alerts.flash')
                             <div class="card-header">
-                                <h4 class="card-title">Edit Company</h4>
+                                <h4 class="card-title">Edit Manufactory</h4>
                             </div>
                                 @if ($errors->any())
                                     <div class="alert alert-danger">
